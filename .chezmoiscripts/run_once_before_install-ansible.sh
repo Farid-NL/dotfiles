c16676157e2cd6fe8797180654dd0cdb06f17edc
Carlos Farid Nogales López <34426099+Farid-NL@users.noreply.github.com>
2024-03-09 17:15:14 -0600

    fix: add '-y' to commands and remove some echoes

--- .chezmoiscripts/run_once_before_install-ansible.sh
+++ .chezmoiscripts/run_once_before_install-ansible.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
 if dpkg --get-selections | grep -wq ansible; then
-  echo -e "Ansible already installed"
-
-  echo "Running Ansible"
+  echo "Ansible already installed"
   ansible-playbook ~/.bootstrap/setup.ansible.yml --ask-become-pass
-  echo "Ansible configuration done"
-
   exit
 fi
 
 echo "Installing Ansible"
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get update
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt-get update -y
 sudo apt-get install -y ansible
 echo -e "Ansible installation complete\n\n"
 
-echo "Running Ansible"
 ansible-playbook ~/.bootstrap/setup.ansible.yml --ask-become-pass
-echo "Ansible configuration done"
