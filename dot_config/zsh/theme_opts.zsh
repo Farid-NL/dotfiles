44605320bef957dbb69b201255860ce0fd54ff56
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2024-05-20 15:28:03 -0600

    chore(zsh): move theme opts to file

---
+++ dot_config/zsh/theme_opts.zsh
@@ -0,0 +1,20 @@
+# Dracula theme for fzf
+export FZF_DEFAULT_OPTS='
+  --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
+  --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+  --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+  --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+  --bind ctrl-space:toggle-out,ctrl-a:toggle-all'
+
+# Dracula theme eza(exa)
+export EXA_COLORS="uu=36:gu=37:sn=32:sb=32:da=34:ur=34:uw=35:ux=36:ue=36:gr=34:gw=35:gx=36:tr=34:tw=35:tx=36"
+
+# Dracula theme for man pages
+export LESS_TERMCAP_mb=$'\e[1;31m'      # begin bold
+export LESS_TERMCAP_md=$'\e[1;34m'      # begin blink
+export LESS_TERMCAP_so=$'\e[01;45;37m'  # begin reverse video
+export LESS_TERMCAP_us=$'\e[01;36m'     # begin underline
+export LESS_TERMCAP_me=$'\e[0m'         # reset bold/blink
+export LESS_TERMCAP_se=$'\e[0m'         # reset reverse video
+export LESS_TERMCAP_ue=$'\e[0m'         # reset underline
+export GROFF_NO_SGR=1                   # for konsole
