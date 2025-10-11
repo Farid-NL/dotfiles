fc1bc0744d5f585b7f9486dbddf8c0b961fdee83
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-10-11 12:40:03 -0600

    fix: use ~/.local/bin for Bitwarden CLI

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -8,6 +8,6 @@ bw_url='https://vault.bitwarden.com/download/?app=cli&platform=linux'
 echo -e '\nInstalling Bitwarden CLI'
 wget -q --show-progress "${bw_url}" -O '/tmp/bw.zip'
 
-sudo unzip -oq '/tmp/bw.zip' -d '/usr/local/bin'
-sudo chmod +x /usr/local/bin/bw
+unzip -oq '/tmp/bw.zip' -d "$HOME/.local/bin"
+chmod +x "$HOME/.local/bin/bw"
 echo -e 'Bitwarden CLI installed\n'
