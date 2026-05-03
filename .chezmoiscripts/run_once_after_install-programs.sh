4fedd86d2f7798f3540fff45267e3e2db01d4909
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2026-05-02 21:22:27 -0600

    fix: use the new sx script from the software installer

--- .chezmoiscripts/run_once_after_install-programs.sh
+++ .chezmoiscripts/run_once_after_install-programs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash <(wget -qO - https://raw.githubusercontent.com/Farid-NL/soft/refs/heads/main/soft) install-all
+bash <(wget -qO - https://raw.githubusercontent.com/Farid-NL/soft/refs/heads/main/sx) install-all
